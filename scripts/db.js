6f9ad79c8ce2cec36e82b7414ae01af25e760bd5
Lukas Strnad <lukas.strnad222@gmail.com>
2024-10-11 03:02:29 +0200

    changed_db

--- scripts/db.js
+++ scripts/db.js
@@ -3,91 +3,91 @@
       "name": "Margherita",
       "price": 7.50,
       "description": "Klassische Pizza mit Tomatensauce, Mozzarella und frischem Basilikum",
-      "amount": 10
+      "amount": 0
     },
     {
       "name": "Salami",
       "price": 9.00,
       "description": "Pizza mit Tomatensauce, Mozzarella und würziger Salami",
-      "amount": 8
+      "amount": 0
     },
     {
       "name": "Funghi",
       "price": 8.50,
       "description": "Pizza mit Tomatensauce, Mozzarella und Champignons",
-      "amount": 12
+      "amount": 0
     },
     {
       "name": "Quattro Formaggi",
       "price": 10.00,
       "description": "Pizza mit vier verschiedenen Käsesorten: Mozzarella, Gorgonzola, Parmesan und Emmentaler",
-      "amount": 6
+      "amount": 0
     },
     {
-      "name": "Hawaiian",
+      "name": "Hawai",
       "price": 9.50,
       "description": "Pizza mit Tomatensauce, Mozzarella, Schinken und Ananas",
-      "amount": 5
+      "amount": 0
     },
     {
-      "name": "Diavola",
+      "name": "Diavolo",
       "price": 9.80,
       "description": "Pizza mit Tomatensauce, Mozzarella und scharfer Salami",
-      "amount": 7
+      "amount": 0
     },
     {
       "name": "Prosciutto",
       "price": 9.20,
       "description": "Pizza mit Tomatensauce, Mozzarella und gekochtem Schinken",
-      "amount": 9
+      "amount": 0
     },
     {
       "name": "Capricciosa",
       "price": 10.50,
       "description": "Pizza mit Tomatensauce, Mozzarella, Schinken, Pilzen, Artischocken und Oliven",
-      "amount": 6
+      "amount": 0
     },
     {
-      "name": "Vegetariana",
+      "name": "Vegetarisch",
       "price": 9.00,
       "description": "Pizza mit Tomatensauce, Mozzarella und verschiedenen frischen Gemüsesorten",
-      "amount": 11
+      "amount": 0
     },
     {
-      "name": "Tonno e Cipolla",
+      "name": "Thunfisch",
       "price": 9.70,
       "description": "Pizza mit Tomatensauce, Mozzarella, Thunfisch und roten Zwiebeln",
-      "amount": 8
+      "amount": 0
     },
     {
       "name": "Calzone",
       "price": 10.00,
       "description": "Gefaltete Pizza mit Tomatensauce, Mozzarella, Schinken und Pilzen",
-      "amount": 4
+      "amount": 0
     },
     {
-      "name": "Rucola e Prosciutto",
+      "name": "Prosciutto",
       "price": 11.00,
       "description": "Pizza mit Tomatensauce, Mozzarella, Rucola und Parmaschinken",
-      "amount": 5
+      "amount": 0
     },
     {
-      "name": "Frutti di Mare",
+      "name": "Meeresfrüchtepizza",
       "price": 12.00,
       "description": "Pizza mit Tomatensauce, Mozzarella und verschiedenen Meeresfrüchten",
-      "amount": 3
+      "amount": 0
     },
     {
       "name": "Bolognese",
       "price": 10.20,
       "description": "Pizza mit Tomatensauce, Mozzarella und Bolognese-Hackfleischsauce",
-      "amount": 7
+      "amount": 0
     },
     {
-      "name": "Speck e Patate",
+      "name": "Speck",
       "price": 10.50,
       "description": "Pizza mit Tomatensauce, Mozzarella, Speck und Kartoffelscheiben",
-      "amount": 6
+      "amount": 0
     }
   ]
   
